47ae22076141759cac637506085f8dc622bcdec2
Yang.Z <33799667+zhybit@users.noreply.github.com>
2020-11-25 16:30:53 -0800

    Update data_load_01.py

--- Zhao_etal_PNAS_2020/code/data_load_01.py
+++ Zhao_etal_PNAS_2020/code/data_load_01.py
@@ -14,7 +14,7 @@ def get_operating_data(num):
     list_operation = []
     for i in range(42):
         list_operation.append('data_operate_' + str(i) + '.csv')
-    file_path= './data/ev_data1/data_vehicle_operation/' + list_operation[num]
+    file_path= './data/ev_data/data_vehicle_operation/' + list_operation[num]
     d1 = pd.read_csv(file_path)
     return d1
 
@@ -22,7 +22,7 @@ def get_operating_data_auto():
     data_op = pd.DataFrame()
     for i in range(42):
         name = 'data_operate_' + str(i) + '.csv'
-        file_path= './data/ev_data1/data_vehicle_operation/' + name
+        file_path= './data/ev_data/data_vehicle_operation/' + name
         d_tmp = pd.read_csv(file_path)
         data_op = pd.concat([data_op, d_tmp])
     return data_op
